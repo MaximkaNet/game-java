61e8e194c5066546b1c9167d248d5dae9e1b18b0
Maksym Zavada <zavadmak@fel.cvut.cz>
2024-04-28 17:59:33 +0200

    Add settings provider

--- src/main/java/cz/cvut/fel/zavadmak/engine/utils/JSONLoader.java
+++ src/main/java/cz/cvut/fel/zavadmak/engine/utils/JSONLoader.java
@@ -2,11 +2,12 @@ package cz.cvut.fel.zavadmak.engine.utils;
 
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class JSONLoader {
-    public static JSONObject load(String pathToFile) throws Exception {
+    public static JSONObject load(String pathToFile) throws IOException {
         String fullFileContent = Files.readString(Paths.get(pathToFile));
         return new JSONObject(fullFileContent);
     }
---
+++ src/main/java/cz/cvut/fel/zavadmak/magic_adventure/helpers/Settings.java
@@ -0,0 +1,77 @@
+package cz.cvut.fel.zavadmak.magic_adventure.helpers;
+
+import cz.cvut.fel.zavadmak.engine.utils.JSONLoader;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+
+public class Settings {
+    private String playerNickname;
+    private final HashMap<Controls, String> controls = new HashMap<>();
+
+    public enum Controls {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    public void setDefault() {
+        controls.put(Controls.UP, "W");
+        controls.put(Controls.DOWN, "S");
+        controls.put(Controls.LEFT, "A");
+        controls.put(Controls.RIGHT, "D");
+    }
+
+    public String getControlKey(Controls controlKey) {
+        return controls.get(controlKey);
+    }
+
+    public void setControl(Controls controlKey, String key) {
+        controls.put(controlKey, key);
+    }
+
+    /**
+     * Load settings from file
+     *
+     * @param filename the settings file
+     * @throws IOException
+     */
+    public void load(String filename) throws IOException {
+        JSONObject loaded = JSONLoader.load(filename);
+        JSONObject jsonControls = loaded.getJSONObject("controls");
+        JSONObject jsonPlayer = loaded.getJSONObject("player");
+        playerNickname = jsonPlayer.getString("nickname");
+
+        controls.put(Controls.UP, jsonControls.getString(Controls.UP.name().toLowerCase()));
+        controls.put(Controls.DOWN, jsonControls.getString(Controls.DOWN.name().toLowerCase()));
+        controls.put(Controls.LEFT, jsonControls.getString(Controls.LEFT.name().toLowerCase()));
+        controls.put(Controls.RIGHT, jsonControls.getString(Controls.RIGHT.name().toLowerCase()));
+    }
+
+    /**
+     * Save settings to file
+     *
+     * @param filename the settings file
+     * @throws IOException throws if file was not write
+     */
+    public void save(String filename) throws IOException {
+        JSONObject jsonControls = new JSONObject();
+        jsonControls.put(Controls.UP.name().toLowerCase(), controls.getOrDefault(Controls.UP, "W"));
+        jsonControls.put(Controls.DOWN.name().toLowerCase(), controls.getOrDefault(Controls.DOWN, "S"));
+        jsonControls.put(Controls.LEFT.name().toLowerCase(), controls.getOrDefault(Controls.LEFT, "A"));
+        jsonControls.put(Controls.RIGHT.name().toLowerCase(), controls.getOrDefault(Controls.RIGHT, "D"));
+
+        JSONObject jsonPlayer = new JSONObject();
+        jsonPlayer.put("nickname", playerNickname);
+
+        JSONObject jsonSettings = new JSONObject();
+        jsonSettings.put("controls", jsonControls);
+        jsonSettings.put("player", jsonPlayer);
+
+        Files.writeString(Path.of(filename), jsonSettings.toString());
+    }
+}
